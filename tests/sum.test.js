ce93656228553fe4fd1d3bedb44b239caae32376
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-07-18 15:06:50 +0530

    Thow error

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,3 +1,12 @@
+// const sum=require('../functions/sum.js')
+
+// test('testing sum function', ()=>{
+//     expect(sum(1,2)).toBe(3);
+// })
+
+
+//Check-2
+
 // test('Object testing',()=>{
 //     const data={one:1};
 //     data['two']=2;
@@ -5,7 +14,17 @@
 // })
 
 
-test('Falsy checking',()=>{
-    const n=undefined;
-    expect(n).toBeFalsy()
-})
+// test('Falsy checking',()=>{
+//     const n=undefined;
+//     expect(n).toBeFalsy()
+// })
+
+function throwError() {
+  throw new Error('Something went wrong');
+}
+
+test('throws an error', () => {
+  expect(throwError).toThrow();                    // ✅ any error
+  expect(throwError).toThrow('Something went wrong'); // ✅ specific message
+  expect(throwError).toThrow(Error);               // ✅ error type
+});
